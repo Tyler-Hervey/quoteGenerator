cde34bb2162118608fffbc1eb24207fe25114644
Tyler Hervey <hervey711@gmail.com>
2022-02-22 22:06:05 -0600

    added spinning loader

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader')
 let apiQuotes = [];
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // get quotes from api
 function newQuote(quotes) {
+    loading();
   let quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = apiQuotes[quoteIndex];
   if (quote.text.length > 50) {
@@ -15,6 +26,7 @@ function newQuote(quotes) {
   } else {
     quoteText.classList.remove("long-quote");
   }
+// set text
   quoteText.textContent = quote.text;
 
   if (quote.author !== null) {
@@ -22,15 +34,18 @@ function newQuote(quotes) {
   } else {
     authorText.textContent = "Unknown";
   }
+// hide loader
+    complete();
+
 }
 
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
-
+  loading();
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    // console.log(apiQuotes);
+    complete();
     newQuote(apiQuotes);
   } catch (e) {
     // catch error here
