f2603ace44060b084bf5a0607251d04ecda150a3
Tyler Hervey <hervey711@gmail.com>
2022-02-22 21:41:19 -0600

    added event listeners to buttons

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
 // get quotes from api
-function newQuote (quotes) {
-    let quoteIndex = Math.floor(Math.random() * quotes.length);
-    const quote = apiQuotes[quoteIndex];
-    console.log(quote);
-    quoteText.textContent = quote.text;
+function newQuote(quotes) {
+  let quoteIndex = Math.floor(Math.random() * quotes.length);
+  const quote = apiQuotes[quoteIndex];
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 
-    if(quote.author !== null) {
-        authorText.textContent = quote.author;
-    }
+  if (quote.author !== null) {
+    authorText.textContent = quote.author;
+  } else {
+    authorText.textContent = "Unknown";
+  }
 }
 
-async function getQuotes () {
-    const apiUrl = 'https://type.fit/api/quotes';
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
 
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes =  await response.json();
-        // console.log(apiQuotes);
-        newQuote(apiQuotes);
-    } catch(e) {
-        // catch error here 
-
-    }
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    // console.log(apiQuotes);
+    newQuote(apiQuotes);
+  } catch (e) {
+    // catch error here
+    console.log(e);
+  }
 }
+
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} – ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', getQuotes);
 
 getQuotes();
